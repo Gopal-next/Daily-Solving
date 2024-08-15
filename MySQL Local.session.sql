ffce100e6f870a2e35efb75c9d9ba9a087a522ac
Gopal Kumar <gk3910598@gmail.com>
2024-08-15 20:04:47 +0530

    first commit

---
+++ MySQL Local.session.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users ( id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL,
+email VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO users (username, email) VALUES
+('john_doe', 'john@example.com'),
+('jane_smith', 'jane@example.com'), ('bob jones', 'bob@example.com');
+
+SELECT * from users;
