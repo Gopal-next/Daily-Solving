42d8eed452ce69975e35a85602a92038386048f1
Gopal Kumar <gk3910598@gmail.com>
2024-08-26 20:18:32 +0530

    First commit

--- Daily Solving/GFG streak/17th_August_2024.py
+++
@@ -1,7 +0,0 @@
-class Solution:   
-    def peakElement(self,arr, n):
-        # Code here
-        maxx = max(arr)
-        for i in range(n):
-            if arr[i] == maxx:
-                return i
