90dd596da0c898fa9653dd380d959b546ed12d82
Gopal Kumar <gk3910598@gmail.com>
2024-08-31 20:28:04 +0530

    Intial project plan

---
+++ Daily Solving/Leetcode/array/Maximum Gap.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+        nums.sort()
+        maxx = float('-inf')
+        for i in range(len(nums)-1):
+            new = nums[i+1] - nums[i]
+            if new  > maxx:
+                maxx= new
+        return maxx
